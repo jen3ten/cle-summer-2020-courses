d7d61bb79277d4ca35ab99d994328ca7f34ecbee
Jen E <jenevans3ten@gmail.com>
2020-06-19 10:24:35 -0400

    WIP NSubstitute added, but controller tests incomplete

--- cle-summer-2020-courses.Tests/CourseControllerTests.cs
+++ cle-summer-2020-courses.Tests/CourseControllerTests.cs
@@ -3,35 +3,39 @@ using cle_summer_2020_courses.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
+using NSubstitute;
+using cle_summer_2020_courses.Repositories;
 
 namespace cle_summer_2020_courses.Tests
 {
     public class CourseControllerTests
     {
-        //CourseController underTest;
+        CourseController underTest;
+        IRepository<Course> courseRepo;
 
-        //public CourseControllerTests()
-        //{
-        //    underTest = new CourseController();
-        //}
-        //[Fact]
-        //public void Index_Returns_A_View()
-        //{
-        //    //var underTest = new CourseController();
+        public CourseControllerTests()
+        {
+            courseRepo = Substitute.For<IRepository<Course>>();
+            underTest = new CourseController(courseRepo);
+        }
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            //var underTest = new CourseController();
 
-        //    var result = underTest.Index();
+            var result = underTest.Index();
 
-        //    Assert.IsType<ViewResult>(result);
-        //}
+            Assert.IsType<ViewResult>(result);
+        }
 
-        //[Fact]
-        //public void Index_Passes_CourseModel_To_View()
-        //{
+        [Fact]
+        public void Index_Passes_CourseModel_To_View()
+        {
 
-        //    var result = underTest.Index();
+            var result = underTest.Index();
 
-        //    Assert.IsType<Course>(result.Model);
-        //}
+            Assert.IsType<Course>(result.Model);
+        }
 
     }
 }
