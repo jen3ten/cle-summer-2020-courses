d1697fbfd8d34f8beb9059d743ad173c7e64d5aa
Jen E <jenevans3ten@gmail.com>
2020-06-19 13:38:39 -0400

    finished adding mocked tests for course index, details

--- cle-summer-2020-courses.Tests/CourseControllerTests.cs
+++ cle-summer-2020-courses.Tests/CourseControllerTests.cs
@@ -5,6 +5,8 @@ using System;
 using Xunit;
 using NSubstitute;
 using cle_summer_2020_courses.Repositories;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace cle_summer_2020_courses.Tests
 {
@@ -19,23 +21,41 @@ namespace cle_summer_2020_courses.Tests
             underTest = new CourseController(courseRepo);
         }
         [Fact]
-        public void Index_Returns_A_View()
+        public void Index_Returns_A_ViewResult()
         {
-            //var underTest = new CourseController();
-
             var result = underTest.Index();
 
             Assert.IsType<ViewResult>(result);
         }
 
         [Fact]
-        public void Index_Passes_CourseModel_To_View()
+        public void Index_Passes_All_Courses_To_View()
         {
-
+            var expectedCourses = new List<Course>();
+            courseRepo.GetAll().Returns(expectedCourses);
+            
             var result = underTest.Index();
 
-            Assert.IsType<Course>(result.Model);
+            Assert.Equal(expectedCourses, result.Model);
         }
 
+        [Fact]
+        public void Details_Returns_a_ViewResult()
+        {
+            var result = underTest.Details(1);
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Details_Passes_Course_To_View()
+        {
+            var expectedCourse = new Course();
+            courseRepo.GetById(1).Returns(expectedCourse);
+            
+            var result = underTest.Details(1);
+            
+            Assert.Equal(expectedCourse, result.Model);
+        }
     }
 }
