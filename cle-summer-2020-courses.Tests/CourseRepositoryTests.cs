cd557a0b975abf0aceb674c7f67fa8b5eb16275b
Jen E <jenevans3ten@gmail.com>
2020-06-19 14:12:35 -0400

    WIP testing repo GetAll returns a collection of Courses

---
+++ cle-summer-2020-courses.Tests/CourseRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using cle_summer_2020_courses.Models;
+using cle_summer_2020_courses.Repositories;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace cle_summer_2020_courses.Tests
+{
+    public class CourseRepositoryTests
+    {
+        private UniversityContext db;
+        private CourseRepository underTest;
+
+        public CourseRepositoryTests()
+        {
+            db = new UniversityContext();
+            underTest = new CourseRepository(db);
+        }
+
+        [Fact]
+        public void GetAll_Returns_List_of_Courses()
+        {
+            var result = underTest.GetAll();
+            
+            Assert.IsType<List<Course>>(result);
+        }
+    }
+}
