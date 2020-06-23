567aa4921801956486cb10f7cf1a58ca8541ce1f
Jen E <jenevans3ten@gmail.com>
2020-06-22 22:42:14 -0400

    added CourseRepo integration test for Create method

---
+++ cle-summer-2020-courses.Tests/CourseRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using cle_summer_2020_courses.Models;
+using cle_summer_2020_courses.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace cle_summer_2020_courses.Tests
+{
+    public class CourseRepositoryTests : IDisposable
+    {
+        private UniversityContext db;
+        private CourseRepository underTest;
+
+        public CourseRepositoryTests()
+        {
+            db = new UniversityContext();
+            db.Database.BeginTransaction();
+
+            underTest = new CourseRepository(db);
+        }
+
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
+
+        [Fact]
+        public void Create_Increases_Count_of_Courses()
+        {
+            var startCount = db.Courses.Count();
+
+            underTest.Create(new Course()
+            {
+                Name = "new Course", 
+                InstructorId = 1
+            });
+            var endCount = db.Courses.Count();
+
+            Assert.Equal(startCount + 1, endCount);
+
+        }
+    }
+}
