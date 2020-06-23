a698e45301340233baf857baa88a46c2d61a075d
Jen E <jenevans3ten@gmail.com>
2020-06-23 18:18:33 -0400

    removed commented code and line breaks

--- cle-summer-2020-courses/Repositories/CourseRepository.cs
+++ cle-summer-2020-courses/Repositories/CourseRepository.cs
@@ -9,26 +9,8 @@ namespace cle_summer_2020_courses.Repositories
 {
     public class CourseRepository : Repository<Course>, IRepository<Course>
     {
-        //private UniversityContext db;
-
         public CourseRepository(UniversityContext db) : base(db)
         {
-            //this.db = db;
         }
-
-        //public void Create(Course course)
-        //{
-        //    db.Courses.Add(course);
-        //    db.SaveChanges();
-        //}
-
-        //public IEnumerable<Course> GetAll()
-        //{
-        //    return db.Courses;
-        //}
-        //public Course GetById(int id)
-        //{
-        //    return db.Courses.Single(c => c.Id == id);
-        //}
     }
 }
--- cle-summer-2020-courses/Repositories/InstructorRepository.cs
+++ cle-summer-2020-courses/Repositories/InstructorRepository.cs
@@ -8,26 +8,8 @@ namespace cle_summer_2020_courses.Repositories
 {
     public class InstructorRepository : Repository<Instructor>, IRepository<Instructor>
     {
-        //private UniversityContext db;
-
         public InstructorRepository(UniversityContext db) : base(db)
         {
-            //this.db = db;
         }
-
-        //public void Create(Instructor obj)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public IEnumerable<Instructor> GetAll()
-        //{
-        //    return db.Instructors;
-        //}
-
-        //public Instructor GetById(int id)
-        //{
-        //    return db.Instructors.Single(i => i.Id == id);
-        //}
     }
 }
