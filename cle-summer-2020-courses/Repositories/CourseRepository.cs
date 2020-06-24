a8e13747f7234c449d53ff9478f09357fb760381
Jen E <jenevans3ten@gmail.com>
2020-06-23 20:43:58 -0400

    added populate instructor list method to course repo

--- cle-summer-2020-courses/Repositories/CourseRepository.cs
+++ cle-summer-2020-courses/Repositories/CourseRepository.cs
@@ -9,8 +9,19 @@ namespace cle_summer_2020_courses.Repositories
 {
     public class CourseRepository : Repository<Course>, IRepository<Course>
     {
+        UniversityContext db;
         public CourseRepository(UniversityContext db) : base(db)
         {
+            this.db = db;
+        }
+
+        public List<Instructor> PopulateInstructorList()
+        {
+            var instructorQuery = from i in db.Instructors
+                                  orderby i.Name
+                                  select i;
+
+            return instructorQuery.ToList();
         }
     }
 }
